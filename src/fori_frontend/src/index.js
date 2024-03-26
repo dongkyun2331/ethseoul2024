c8d8078b01fd2a8893ef06b6978306bdd3b4a6b1
dongkyun2331 <dongkyun2331@gmail.com>
2024-03-26 22:44:02 +0900

    Initial commit.

---
+++ src/fori_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { fori_backend } from "../../declarations/fori_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await fori_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
